7e67cad1d2b5391b3c00a1aba67f75433531130b
Fyodor Perejoguine <fyodor.perejoguine@gmail.com>
2019-04-15 19:13:32 +0200

    Implement more general and parametrizable backoff handling.

--- src/main.ts
+++ src/main.ts
@@ -24,19 +24,31 @@ export class RequestHandler {
     // Set rate limit to 100 requests per second (+ 5ms to be safe).
     private rateLim: number = 100; // Maximum allowed nr. of request in period.
     private periodLength: number = 1005; // Length of period for which rateLim applies.
-    private retry = 3;
-    private trans = (w: number) => w * 2;
+
+    // Backoff function. Takes number of previous attempts and error and calculates returns either wait time or undefined.
+    private getBackoff: (error: object, attemptNr: number) => number | undefined; // Backoff function on error
 
     // Number of requests made in the last period.
     private requestsLastPeriod: number = 0;
     // Queue of requests. Should be empty if requestsLastPeriod < rateLim
     private queue: Request[] = [];
 
-    constructor(rateLim: number, periodLength: number, trans?: (x: number) => number, retry?: number) {
+    // Backoff is either function that determines how long to wait for the next retry or number denoting the number of retries to make.
+    constructor(rateLim: number, periodLength: number, backoff: ((error: object, attemptNr: number) => number | undefined) | number = 3) {
         this.rateLim = rateLim;
         this.periodLength = periodLength;
-        if(trans) this.trans = trans;
-        if(retry) this.retry = retry;
+
+        if (typeof(backoff) === 'number') {
+            this.getBackoff = (error: object, attemptNr: number) => {
+                if (attemptNr < backoff) {
+                    return periodLength * (2 ** attemptNr);
+                } else {
+                    return undefined;
+                }
+            }
+        } else {
+            this.getBackoff = backoff;
+        }
     }
 
     /**
@@ -85,18 +97,19 @@ export class RequestHandler {
      * @param retry 
      * @param trans 
      */
-    private async sendReq(request: requestFunc, wait: number = this.periodLength, retry = this.retry): Promise<object> {
+    private async sendReq(request: requestFunc, attemptNr: number = 0): Promise<object> {
         try {
             const reqPromise = request();
             this.blockRequests();
             return await reqPromise;
         } catch(e) {
-            // If error, halt everything and retry 3 times with increasing intervals
-            // Halt new requests from being executed, wait for x amount of ms, then retry y amount, while transforming
-            // x with certain function. If unsuccessful after last try pass error upwards
-            if (retry > 0) {
+            // If error, execute function to calculate backoff time.
+            // Halt new all new requests from being executed, wait for x amount of ms, then retry y amount, while transforming
+            // x with certain function. If unsuccessful after last try pass error upwards.
+            const wait = this.getBackoff(e, attemptNr);
+            if (wait) {
                 await this.blockRequests(this.rateLim, wait, () => {});
-                const result = await this.sendReq(request, this.trans(wait), --retry);
+                const result = await this.sendReq(request, attemptNr + 1);
                 this.checkQueue();
                 return result;
             } else {
